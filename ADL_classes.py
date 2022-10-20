04f7e60d8b681bec874869f0618c215981714000
agrodl_main <111681601+agrodlmainuser@users.noreply.github.com>
2022-10-20 12:20:06 +0300

    Update ADL_classes.py
    Added ADL_gh class

--- ADL_classes.py
+++ ADL_classes.py
@@ -1,5 +1,9 @@
+
 import xml.etree.ElementTree as ET
 import PIL
+import geopy.distance
+import numpy as np
+
 
 class ADL_Read_XML:
 
@@ -45,7 +49,92 @@ class ADL_EXIF:
     gps_lat = float(EXIF_data[34853][2][0][0])
     gps_long = float(EXIF_data[34853][4][0][0])
     return (gps_lat/1000000, gps_long/1000000)
-    
 
 
-             
+class ADL_gh:
+
+  def __init__(self, gh_corners_coordinates):
+    self.gh_corners_coordinates = gh_corners_coordinates
+
+  def setup(self):
+    # comlete missing corner in case of input of 3
+    if len(self.gh_corners_coordinates) == 3:
+      rr = np.array(self.gh_corners_coordinates)
+      a = geopy.distance.distance(rr[0], rr[1]).km
+      b = geopy.distance.distance(rr[0], rr[2]).km
+      c = geopy.distance.distance(rr[1], rr[2]).km
+
+      if a > b and a > c:
+        self.max_dist = a
+        flag = 2
+        self.corner = rr[2]
+        if b > c:
+          self.near_corner = rr[1]
+          self.far_corner = rr[0]
+        else:
+          self.near_corner = rr[0]
+          self.far_corner = rr[1]
+      elif b > a and b > c:
+        self.max_dist = b
+        flag = 1
+        self.corner = rr[1]
+        if a > c:
+          self.near_corner = rr[2]
+          self.far_corner = rr[0]
+        else:
+          self.near_corner = rr[0]
+          self.far_corner = rr[2]    
+      else:
+        self.max_dist = c
+        flag = 0
+        self.corner = rr[0]
+        if a > b:
+          self.near_corner = rr[2]
+          self.far_corner = rr[1]
+        else:
+          self.near_corner = rr[1]
+          self.far_corner = rr[2]
+      fourth_corner = self.near_corner - self.corner + self.far_corner
+      self.fourth_corner = tuple(fourth_corner)
+      # adding fourth corner to the input coordinates list
+      self.gh_corners_coordinates.append(self.fourth_corner)
+    # in case of input of 4
+    else:
+      corner = self.gh_corners_coordinates[0]
+      near_corner = self.gh_corners_coordinates[0]
+      far_corner = self.gh_corners_coordinates[0]
+      fourth_corner = self.gh_corners_coordinates[0]
+  
+  def check_if_in_gh(self, point):
+    n_points = [self.gh_corners_coordinates[0][0], self.gh_corners_coordinates[1][0], 
+                self.gh_corners_coordinates[2][0], self.gh_corners_coordinates[3][0]]
+    e_points = [self.gh_corners_coordinates[0][1], self.gh_corners_coordinates[1][1],
+                self.gh_corners_coordinates[2][1], self.gh_corners_coordinates[3][1]]
+    for i in range(4):
+      dist = geopy.distance.distance(point, self.gh_corners_coordinates[i]).km
+      # checking the distance from the current corner to the provided point
+      # cheching if the provided point in out of the max/min limits from both north/south and east/west
+      if dist > 0.9 * self.max_dist or point[0] > max(n_points) or point[0] < min(n_points) or point[1] > max(e_points) or point[1] < min(e_points):
+        return False 
+        break
+      else:
+        continue 
+    return True 
+  
+  def line_mapping(self, lines_cor):
+    lines_cor.sort(key = lambda x: float(x[1]))
+    line_index = 1
+    mapping_dict = {}
+    for i, line_cor in enumerate(lines_cor):
+      if i == 0 :
+        mapping_dict[f"line_{line_index}"] = line_cor
+        prev_cor = line_cor 
+        line_index += 1
+      else:
+        if line_cor[0] == prev_cor[0] and line_cor[1] == prev_cor[1]:
+          continue
+        else:
+          mapping_dict[f"line_{line_index}"] = line_cor
+          line_index += 1
+          prev_cor = line_cor 
+    self.gh_lines = mapping_dict
